28bae1d8c9aa45eeabf7523e977f2893886735a0
ArtemSharypov <artem.sharypov@gmail.com>
2017-12-23 20:36:42 -0600

    Added SMS messaging
    -Added twilio fully to send messages as requested to the desired location with the desired message

--- Backend Server/api/index.js
+++ Backend Server/api/index.js
@@ -1,18 +1,26 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
+var keys = require("./twilioKeys");
+var client = require('twilio')(keys.accountSid, keys.authToken)
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
-
 
 app.post("/messages", function(req, res){
     if(!req.body.phoneNumber || typeof req.body.phoneNumber != "string" || !req.body.message || typeof req.body.message != "string"){
         res.status(400).send("400 Bad Request");
     }
 
+    console.log(keys.sendingNumber);
     console.log(req.body.phoneNumber);
     console.log(req.body.message);
+
+    client.messages.create({
+        from: keys.sendingNumber,
+        to: req.body.phoneNumber,
+        body: req.body.message
+    }).then((message) => console.log(message.sid));
 
     res.status(200).end();
 });
